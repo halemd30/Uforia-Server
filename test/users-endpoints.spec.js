f0fdf781eac03d1cce84b6ec5a8e4fbbae10b156
Mac Hale <machaledesign@gmail.com>
2020-08-20 22:37:29 -0600

    adds tests

--- test/users-endpoints.spec.js
+++ test/users-endpoints.spec.js
@@ -48,6 +48,118 @@ describe("Users Endpoints", function () {
             });
         });
       });
+      it(`responds 400 'Password must be longer than 7 characters' when empty password`, () => {
+        const userShortPassword = {
+          username: "test_username",
+          password: "asdf13",
+          phone_number: "1231231234",
+        };
+
+        return supertest(app)
+          .post("/api/users")
+          .send(userShortPassword)
+          .expect(400, {
+            error: "Password must be at least 8 characters",
+          });
+      });
+
+      it(`responds 400 when password is longer than 72 characters`, () => {
+        const passwordTooLong = {
+          username: "test_user",
+          password: "Aasdf123",
+          phone_number: "1231231234",
+        };
+
+        return supertest(app)
+          .post("/api/users")
+          .send(passwordTooLong)
+          .expect(400, {
+            error: "Password must be less than 72 characters",
+          });
+      });
+
+      it(`responds 400 when password starts with spaces`, () => {
+        const passwordStartsWithSpaces = {
+          username: "test_user",
+          password: " Asdf1234",
+          phone_number: "1231231234",
+        };
+
+        return supertest(app)
+          .post("/api/users")
+          .send(passwordStartsWithSpaces)
+          .expect(400, {
+            error: "Password must not start or end with empty spaces",
+          });
+      });
+
+      it(`responds 400 when password ends with spaces`, () => {
+        const passwordEndsWithSpaces = {
+          username: "test_user",
+          password: "Asdf1324 ",
+          phone_number: "1231231234",
+        };
+
+        return supertest(app)
+          .post("/api/users")
+          .send(passwordEndsWithSpaces)
+          .expect(400, {
+            error: "Password must not start or end with empty spaces",
+          });
+      });
+
+      it(`responds 400 when username is already taken`, () => {
+        const duplicateUser = {
+          username: "test_user",
+          password: "Asdf1324",
+          phone_number: "1231231234",
+        };
+
+        return supertest(app)
+          .post("/api/users")
+          .send(duplicateUser)
+          .expect(400, {
+            error: "Username already taken",
+          });
+      });
+    });
+
+    context("Happy path", () => {
+      it(`responds 201, serialized user, storing bcrypted password`, () => {
+        const newUser = {
+          username: "test_user",
+          password: "Asdf1234",
+          phone_number: "1231231234",
+        };
+
+        return supertest(app)
+          .post("/api/users")
+          .send(newUser)
+          .expect(201)
+          .expect((res) => {
+            expect(res.body).to.have.property("id");
+            expect(res.body.username).to.eql(newUser.username);
+            expect(res.body.phone_number).to.eql(newUser.phone_number);
+            expect(res.body).to.not.have.property("password");
+            expect(res.headers.location).to.eql(`/api/users/${res.body.id}`);
+          })
+          .expect((res) =>
+            db
+              .from("users")
+              .select("*")
+              .where({ id: res.body.id })
+              .first()
+              .then((row) => {
+                expect(row.username).to.eql(newUser.username);
+                expect(row.phone_number).to.eql(newUser.phone_number);
+
+                return bcrypt.compare(newUser.password, row.password);
+              })
+              .then((compareMatch) => {
+                expect(compareMatch).to.be.true;
+              })
+          );
+      });
     });
   });
 });
