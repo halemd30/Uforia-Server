239cdfb1477eb03912fcfb499bac99fdbbe9ae82
Mac Hale <machaledesign@gmail.com>
2020-08-22 11:52:56 -0600

    refactors

--- src/tasks/tasks-router.js
+++ src/tasks/tasks-router.js
@@ -6,38 +6,44 @@ const { requireAuth } = require("../middleware/jwt-auth");
 
 const tasksRouter = express.Router();
 
-tasksRouter.route("/").post(requireAuth, (req, res, next) => {
-  const { name, start_time, duration, category } = req.body;
-  const newTask = {
-    user_id: req.user.id,
-    name,
-    start_time,
-    duration,
-    category,
-  };
+tasksRouter
+  .route("/")
+  .all(requireAuth)
+  .get((req, res, next) => {
+    const db = req.app.get("db");
+    const userId = req.user.id;
 
-  for (const [key, value] of Object.entries(newTask))
-    if (value == null)
-      return res.status(400).json({
-        error: { message: `Missing '${key}' in request body` },
-      });
+    TasksService.getByUserId(db, userId)
+      .then((tasks) => {
+        res.json(tasks.map(TasksService.serializeTask));
+      })
+      .catch(next);
+  })
+  .post((req, res, next) => {
+    const { name, start_time, duration, category } = req.body;
+    const newTask = {
+      user_id: req.user.id,
+      name,
+      start_time,
+      duration,
+      category,
+    };
 
-  TasksService.insertTask(req.app.get("db"), newTask)
-    .then((task) => {
-      res
-        .status(201)
-        .location(path.posix.join(req.originalUrl, `/${task.id}`))
-        .json(TasksService.serializeTask(task));
-    })
-    .catch(next);
-});
+    for (const [key, value] of Object.entries(newTask))
+      if (value == null)
+        return res.status(400).json({
+          error: { message: `Missing '${key}' in request body` },
+        });
 
-tasksRouter.route("/:userId").get((req, res) => {
-  const id = req.params.userId;
-  TasksService.getByUserId(req.app.get("db"), id).then((userTasks) => {
-    res.json(userTasks.map(TasksService.serializeTask));
+    TasksService.insertTask(req.app.get("db"), newTask)
+      .then((task) => {
+        res
+          .status(201)
+          .location(path.posix.join(req.originalUrl, `/${task.id}`))
+          .json(TasksService.serializeTask(task));
+      })
+      .catch(next);
   });
-});
 
 tasksRouter
   .route("/:taskId")
@@ -57,14 +63,14 @@ tasksRouter
       .catch(next);
   });
 
-tasksRouter.route("/end/:taskId").patch((req, res) => {
+tasksRouter.route("/end/:taskId").patch(requireAuth, (req, res) => {
   const id = req.params.taskId;
   TasksService.updateEndTask(req.app.get("db"), id, {
     end_date: new Date(),
   }).then(res.status(204).end());
 });
 
-tasksRouter.route("/:taskId/modify").patch((req, res) => {
+tasksRouter.route("/:taskId/modify").patch(requireAuth, (req, res) => {
   const id = req.params.taskId;
   TasksService.findTaskAndReturn(req.app.get("db"), id).then(([task]) => {
     TasksService.updateStreak(req.app.get("db"), id, {
@@ -73,7 +79,7 @@ tasksRouter.route("/:taskId/modify").patch((req, res) => {
   });
 });
 
-tasksRouter.route("/reset/:taskId").patch((req, res) => {
+tasksRouter.route("/reset/:taskId").patch(requireAuth, (req, res) => {
   const id = req.params.taskId;
   TasksService.updateResetTask(req.app.get("db"), id, {
     streak: 0,
